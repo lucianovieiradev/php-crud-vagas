29c1e9117d94a57cfb22e97647dd916227125b10
lucianovieiradev <lucianovieiradev@gmail.com>
2023-02-27 22:19:49 -0300

    feat: db connection with PDO.

---
+++ app/Db/Database.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Db;
+
+class Database
+{
+    const HOST = 'localhost';
+    const NAME = 'ldev_vagas';
+    const USER = 'root';
+    const PASS = 'b1n0xd95';
+
+    private $table;
+    private $connection;
+}
